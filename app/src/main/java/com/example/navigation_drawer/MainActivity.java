a5dfbd94bb01dcdd24050672b89a738769a4d2c8
inonanothplanet <143319657+hebuintrde@users.noreply.github.com>
2024-05-15 04:22:02 +0200

    First Version_Week3

---
+++ app/src/main/java/com/example/navigation_drawer/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.navigation_drawer;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout cekmece;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cekmece=findViewById(R.id.cekmece_arkapalan);
+
+
+    }
+    //baskasayfa da da kullnamak istiyoeum
+    public void MenuyeTiklama (View view)
+    {
+        cekm
+    }
+}
